d474fc3fd9372484e7c3b7c7b2c9a7e77d7566d8
Justin Aiken <60tonangel@gmail.com>
2017-04-26 12:16:13 -0700

    AJAX those authors

--- app/assets/javascripts/components/app.js.jsx
+++ app/assets/javascripts/components/app.js.jsx
@@ -1,4 +1,32 @@
+function AuthorsList(props) {
+  return (
+    <ul className="authors-list">
+      {props.authors.map((author, index) =>
+        <li key={author.id} className="author-item">
+          <div className="author-id">{index + 1}</div>
+          <ul className="space-list-items">
+            <li>{author.first_name} {author.last_name}</li>
+          </ul>
+        </li>
+      )}
+    </ul>
+  )
+}
+
 class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      authors: null
+    }
+  }
+
+  componentDidMount() {
+    $.getJSON('/authors.json', (response) => {
+      this.setState({authors: response})
+    });
+  }
+
   render() {
     return (
       <div className="container">
@@ -6,6 +34,10 @@ class App extends React.Component {
           Quick Reads for {this.props.name}
         </h1>
         <Directory />
+        {!this.state.authors
+          ? <p>Loading...</p>
+          : <AuthorsList authors={this.state.authors}/>
+        }
       </div>
     )
   }
